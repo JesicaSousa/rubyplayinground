e25e3dc4b2b46e0ba840e52bab9d121dd69e6763
Jesica Sousa <89730976+JesicaSousa@users.noreply.github.com>
2023-03-13 12:40:10 -0300

    alteração projeto cpf

--- CURSO2/desafio4.rb
+++ CURSO2/desafio4.rb
@@ -1,13 +1,19 @@
 require "cpf_cnpj"
-puts "Insira o seu CPF, sem traços, pontos ou espaço: "
-a = gets.chomp
-cpf =  CPF.new(a)
-cpf.formatted
-val =  cpf.valid?
 
-if val == true
-    print "O CPF digitado é válido."
-else
-    print "O CPF digitado é inválido."
+    loop do
+    puts "Insira o seu CPF, sem traços, pontos ou espaço: "
+    a = gets.chomp
+    cpf =  CPF.new(a)
+    cpf.formatted
+    val =  cpf.valid?
+
+    if val == true
+        print "CPF válido"
+        break
+    else 
+        print  "CPF inválido, tente novamente.\n"
+    end
 end
+
+
 
